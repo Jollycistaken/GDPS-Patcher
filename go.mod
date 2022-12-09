866029f2b7bd7175dd50802bed723f995f81f691
Jollyc <jollybusiness4@gmail.com>
2022-12-09 13:28:55 -0500

    Remade the whole thing

---
+++ go.mod
@@ -0,0 +1,15 @@
+module gdpsPatcher
+
+go 1.19
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/sqweek/dialog v0.0.0-20220809060634-e981b270ebbf
+)
+
+require (
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,102 @@
 package main
 
 import (
-	b64 "encoding/base64"
+	"encoding/base64"
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/sqweek/dialog"
 	"os"
 	"strings"
 )
 
-func main() {
-	test := os.Args[2:]
-	if len(test) == 0 {
-		fmt.Println("No arguments provided or not enough arguments provided")
+func makeError(err string) {
+	color.Red(err)
+	fmt.Println("Press enter to exit...")
+	_, err2 := fmt.Scanln()
+	if err2 != nil {
+		color.Red(err2.Error())
 		os.Exit(1)
 	}
-	file, err := os.Open(os.Args[1])
+	os.Exit(1)
+}
+
+func main() {
+	filename, err := dialog.File().Filter("Executables (*.exe)", "exe").Title("Select the GD exe that you want to patch").Load()
 	if err != nil {
-		panic(err)
+		makeError(err.Error())
+	}
+
+	var url string
+	fmt.Printf("Enter your GDPS url: ")
+	_, err = fmt.Scanln(&url)
+	if err != nil {
+		makeError(err.Error())
+	}
+
+	url = strings.Replace(url, "http://", "", -1)
+	url = strings.Replace(url, "https://", "", -1)
+	if last := len(url) - 1; last >= 0 && url[last] == '/' {
+		url = url[:last]
+	}
+	b64Url := base64.StdEncoding.EncodeToString([]byte("http://" + url))
+	file, err := os.Open(filename)
+	if err != nil {
+		makeError(err.Error())
 	}
 	stat, err := file.Stat()
 	if err != nil {
-		panic(err)
+		err2 := file.Close()
+		if err2 != nil {
+			makeError(err2.Error())
+		}
+		makeError(err.Error())
 	}
 	buffer := make([]byte, stat.Size())
 	_, err = file.Read(buffer)
 	if err != nil {
-		panic(err)
+		err2 := file.Close()
+		if err2 != nil {
+			makeError(err2.Error())
+		}
+		makeError(err.Error())
 	}
-	url := strings.Replace(os.Args[2], "http://", "", -1)
-	yoururl := b64.StdEncoding.EncodeToString([]byte("http://" + url))
-	var EZ string = string(buffer)
-	EZ = strings.Replace(EZ, "www.boomlings.com/database", url, -1)
-	EZ = strings.Replace(EZ, "aHR0cDovL3d3dy5ib29tbGluZ3MuY29tL2RhdGFiYXNl", yoururl, -1)
+	exeBody := string(buffer)
+	if strings.Contains(url, "/database") {
+		if len(url) != len("www.boomlings.com/database") {
+			makeError("Hex editing would corrupt the exe because your url is either too short or long!")
+		}
+		exeBody = strings.Replace(exeBody, "www.boomlings.com/database", url, -1)
+		exeBody = strings.Replace(exeBody, "aHR0cDovL3d3dy5ib29tbGluZ3MuY29tL2RhdGFiYXNl", b64Url, -1)
+	} else {
+		if len(url) != len("www.boomlings.com") {
+			makeError("Hex editing would corrupt the exe because your url is either too short or long!")
+		}
+		exeBody = strings.Replace(exeBody, "www.boomlings.com", url, -1)
+		exeBody = strings.Replace(exeBody, "aHR0cDovL3d3dy5ib29tbGluZ3MuY29t", b64Url, -1)
+	}
+	err = file.Close()
+	if err != nil {
+		makeError(err.Error())
+	}
 
-	file.Close()
-	err = os.Remove(os.Args[1])
+	file, err = os.Create(strings.Replace(filename, ".exe", "_Patched.exe", -1))
 	if err != nil {
-		os.Exit(1)
+		makeError(err.Error())
 	}
-	file, err = os.Create(os.Args[1])
+	_, err = file.WriteString(exeBody)
 	if err != nil {
-		os.Exit(1)
+		makeError(err.Error())
 	}
-	file.WriteString(EZ)
-	file.Close()
-	println("Made GDPS successfully!")
-	println("Press enter to exit...")
-	fmt.Scanln()
+	err = file.Close()
+	if err != nil {
+		makeError(err.Error())
+	}
+
+	color.Green("Successfully patched!")
+	fmt.Println("Press enter to exit...")
+	_, err = fmt.Scanln()
+	if err != nil {
+		makeError(err.Error())
+	}
 	os.Exit(0)
 }
